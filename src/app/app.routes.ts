033595071e7411b1a9e090f0712ec375f1760057
Dalil MAKSENE <dmaksene@turgot-paris.si>
2025-11-18 11:30:31 +0100

    feat(app) : Add routes in router

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,9 @@
 import { Routes } from '@angular/router';
+import { DoctorsPageComponent } from './pages/doctors-page/doctors-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'doctors', component: DoctorsPageComponent},
+    {path: '', component: DoctorsPageComponent},
+    {path: '**', redirectTo: ''}
+];
 
